8e687cc0ba67a5bc7e9af7bc4c16df30ebd1ea08
unknown <ishika@orientaloutsourcing.com>
2023-04-13 10:56:58 +0530

    localMarket

--- src/controllers/localMarket/localMarket.js
+++ src/controllers/localMarket/localMarket.js
@@ -5,67 +5,76 @@ const pool = require("../../connection/db");
 const { send_sqlError, send_response } = require("../../config/reponseObject");
 const LocalMarket = async (req, res, next) => {
     const {
-        OK_AND_COMPLETED,
-      } = require("../../config/const");
+        OK_AND_COMPLETED, INTERNAL_SERVER_ERROR,
+    } = require("../../config/const");
     // app.post('/localMarket', async (req, res) => {
     var zip = req.body.zip
     var distance = req.body.distance
+    try {
     const result = await axios({
         url: `https://www.cars.com/shopping/results/?&maximum_distance=${distance}&zip=${zip}`,
         method: "GET",
     });
     var $ = cheerio.load(result.data);
-    const cars = [];
 
+    var data = $('.listings-page').attr('data-site-activity')
+    var r = JSON.parse(data);
+    var resp = r.vehicleArray
+    const cars = [];
     $('.vehicle-card').each((i, el) => {
         const car = {};
-        car.title = $(el).find('.title').text().trim();
-        car.price = $(el).find('.primary-price').text();
-        car.mileage = $(el).find('.mileage').text();
-        car.rating = $(el).find('.sds-rating__count').text();
-        car.id = $(el).attr('data-listing-id')
-        cars.push(car);
-    });
-    var detail = ''
-    const data = [];
-     cars.map(async (item) => {
-        detail = await axios({
-            url: `https://www.cars.com/vehicledetail/${item.id}/`,
-            method: "GET",
-            // data,
-            // body:JSON.stringify(data),
-        }),
-            $ = cheerio.load(detail.data),
-            $(".basics-section .fancy-description-list").each((index, element) => {
-                const carsDetails = {};
-                if (index === 0) {
-                    // carsDetails.id = item.id
-                    item.exterior_color = $(element).find('dd:nth-child(2)').text().trim()
-                    item.interior_color = $(element).find('dd:nth-child(4)').text().trim()
-                    item.drivetrain = $(element).find('dd:nth-child(6)').text().trim()
-                    item.MPG = $(element).find('dd:nth-child(8)').text().trim()
-                    item.Fuel_type = $(element).find('dd:nth-child(10)').text().trim()
-                    item.Transmission = $(element).find('dd:nth-child(12)').text().trim()
-                    item.Engine = $(element).find('dd:nth-child(14)').text().trim()
-                    item.VIN = $(element).find('dd:nth-child(16)').text().trim()
-                    data.push(item)
-                }
-            })
-        return data
+        car.img = $(el).find('.vehicle-image').attr('data-src') || $(el).find('.vehicle-image').attr('src')
+        cars.push(car)
     })
-    console.log(cars)
-    console.log(data);
-    // d.then(function(res){
-    //     console.log(res);
+    const engines = [];
+    // resp.forEach(async (i,index) => {
+    //     const ress = await axios({
+    //         url: `https://www.cars.com/vehicledetail/${i.listing_id}/`,
+    //         method: "GET",
+    //     });
+    //     var $ = cheerio.load(ress.data);
+    //     $('.fancy-description-list').each((indexs, el) => {
+    //         const detail = {};
+    //         i.engine = $(el).find('dd:nth-child(12)').text()
+    //         cars.push(i)
+    //     })
+    //     // engines.forEach((j,ind) => {
+    //     //     if (index == ind)
+    //     //         i.engine = j.engine
+    //     //     // console.log(resp);
+    //     // })
     // })
+    resp.forEach((i, index) => {
+        cars.forEach((j, ind) => {
+            if (index == ind)
+                i.image = j.img
+        })
+    })
+    const data1 = [];
+    $('.listings-page').each((i, el) => {
+        const d1={}
+         d1.engine = $(el).find('script').text().trim()
+        data1.push(d1)
+    })
+    console.log(data1);
     const obj = {
         res,
         status: true,
-        code:OK_AND_COMPLETED,
-        data:cars,
-        message:"Data Found Successfully"
-      };
-      return send_response(obj);
+        code: OK_AND_COMPLETED,
+        data: resp,
+        message: "Data Found Successfully"
+    };
+    return send_response(obj);
+}catch(err){
+    const obj = {
+        res,
+        status: false,
+        code: INTERNAL_SERVER_ERROR,
+        data: ['Internal Error'],
+    };
+    return send_response(obj);
+}
+    
     
 }
 module.exports = {
